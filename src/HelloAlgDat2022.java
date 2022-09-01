9dfb82cdd7f941eacde4fb2ab0569870c756e91f
vetletj <vetle.tjohansen@gmail.com>
2022-09-01 16:48:42 +0200

    Initial commit

---
+++ src/HelloAlgDat2022.java
@@ -0,0 +1,12 @@
+public class HelloAlgDat2022 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Teller til ti:");
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+//Source code for lectures and the exercises Algorithms and datastructures course at OsloMet 2022
